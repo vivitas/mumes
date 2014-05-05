cb24a31484b9ff2831bf200a3249ef05127559c6
vivitas <fvivitas@gmail.com>
2014-05-05 23:41:00 +0200

    Added devIl
    added developer image libraries and build most simple project with it.

---
+++ mumes/main.c
@@ -0,0 +1,12 @@
+#include "IL\il.h"
+#include "IL\ilu.h"
+#include "IL\ilut.h"
+
+int main()
+{
+	ilInit();
+	iluInit();
+	ilutInit();
+	//ILuint my_image = ilGenImage();
+	//ilLoadImage("J:\resources\exr\location_1_1_hdr.exr");
+}
