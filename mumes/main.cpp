c7be3033ec7ba7f4dedf6e3b8e9093fe48c985e9
vivitas <fvivitas@gmail.com>
2014-05-08 17:01:21 +0200

    Added CPU filter

--- mumes/main.cpp
+++ mumes/main.cpp
@@ -2,13 +2,15 @@
 #include "IL\ilu.h"
 #include "IL\ilut.h"
 #include <iostream>
+
+#define CORDS(X, Y, Z) ((X) + (Y)*width + (Z)*width*height)
 using namespace std;
 struct t_RGBA
 {
-	float r;
-	float g;
-	float b;
-	float a;
+	float r = 0;
+	float g = 0;
+	float b = 0;
+	float a = 0;
 };
 void
 load_image
@@ -68,11 +70,11 @@ get_raw_rgba
 void
 set_raw_rgba
 (
-ILuint image,
-t_RGBA *raw,
-int width,
-int height,
-int depth
+	ILuint image,
+	t_RGBA *raw,
+	int width,
+	int height,
+	int depth
 )
 {
 	ilBindImage(image);
@@ -82,7 +84,77 @@ int depth
 	ilSetPixels(0, 0, 0, width, height, depth, IL_RGBA, IL_FLOAT, raw);
 	return;
 }
-
+void
+increment
+(
+	t_RGBA &dest,
+	t_RGBA src,
+	float coef
+)
+{
+	dest.r += src.r / coef;
+	dest.g += src.g / coef;
+	dest.b += src.b / coef;
+	dest.a += src.a / coef;
+}
+t_RGBA
+cpu_avg_from_to
+(
+	t_RGBA *data,
+	int x_from,
+	int x_to,
+	int y_from,
+	int y_to,
+	int z,
+	int width,
+	int height,
+	int depth
+)
+{
+	t_RGBA result;
+	int number = (x_to - x_from + 1) * (y_to - y_from + 1);
+	for (int x = x_from; x <= x_to; ++x)
+	{
+		for (int y = y_from; y <= y_to; ++y)
+		{
+			increment(result, data[CORDS(x, y, z)], number);
+		}
+	}
+	return result;
+}
+void
+cpu_filter
+(
+	t_RGBA *raw,
+	int width,
+	int height,
+	int depth
+)
+{
+	t_RGBA *buffer = new t_RGBA[width*height*depth];
+	for (int z = 0; z < depth; ++z)
+	{
+		for (int y = 0; y < height; ++y)
+		{
+			for (int x = 0; x < width; ++x)
+			{
+				int y_from = max(y - 1, 0);
+				int x_from = max(x - 1, 0);
+				int y_to = min(y + 1, height - 1);
+				int x_to = min(x + 1, width - 1);
+				buffer[CORDS(x, y, z)] = cpu_avg_from_to(raw, x_from, x_to, y_from, y_to, z, width, height, depth);
+			}
+		}
+		for (int y = 0; y < height; ++y)
+		{
+			for (int x = 0; x < width; ++x)
+			{
+				raw[CORDS(x, y, z)] = buffer[CORDS(x, y, z)];
+			}
+		}
+	}
+	delete[] buffer;
+}
 int main()
 {
 
@@ -107,7 +179,7 @@ int main()
 
 		int width, height, depth;
 		get_raw_rgba(cpu_image, cpu_raw, width, height, depth);
-		
+		cpu_filter(cpu_raw, width, height, depth);
 		set_raw_rgba(cpu_image, cpu_raw, width, height, depth);
 		
 
@@ -116,7 +188,6 @@ int main()
 		save_image(cpu_image, "J:/resources/exr_out/location_1_1_hdr_cpu.exr");
 
 		delete[] cpu_raw;
-		delete[] cuda_raw;
 	}
 	catch (char* c)
 	{
