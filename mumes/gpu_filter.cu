fda371747ee68e29eb091a9d9fd4f6567aa1e603
vivitas <fvivitas@gmail.com>
2014-05-10 00:23:20 +0200

    Set up threads per block

--- mumes/gpu_filter.cu
+++ mumes/gpu_filter.cu
@@ -2,6 +2,9 @@
 #include "cuda_runtime.h"
 #include <stdio.h>
 #include "ScopeTimer.h"
+
+#define TPB 64
+
 static char error[1024];
 static void HandleError(cudaError_t err,
                         const char *file,
@@ -20,34 +23,40 @@ static void HandleError(cudaError_t err,
 __global__ void
 gpu_kernel
 (
-    float *src,
-    float *dst,
+    t_RGBA *src,
+    t_RGBA *dst,
     int width,
     int height,
     int depth
 )
 {
-    if(blockIdx.x >= width || blockIdx.y >= height || threadIdx.x >= 4)
+    int real_x = blockIdx.x;
+    int real_y = blockIdx.y * TPB + threadIdx.x;
+    if(real_x >= width || real_y >= height)
         return;
-    int x_from = MAX(blockIdx.x - 1, 0);
-    int y_from = MAX(blockIdx.y - 1, 0);
-    int x_to = MIN(blockIdx.x + 1, width - 1);
-    int y_to = MIN(blockIdx.y + 1, height - 1);
+    int x_from = MAX(real_x - 1, 0);
+    int y_from = MAX(real_y - 1, 0);
+    int x_to = MIN(real_x + 1, width - 1);
+    int y_to = MIN(real_y + 1, height - 1);
     int coef = (x_to - x_from + 1) * (y_to - y_from - 1);
-    dst[threadIdx.x + 4 * blockIdx.x + 4 * width*blockIdx.y] = 0.f;
  
-    float b = 0;
-    
+    dst[real_x + width * real_y].r = 0.f;
+    dst[real_x + width * real_y].g = 0.f;
+    dst[real_x + width * real_y].b = 0.f;
+    dst[real_x + width * real_y].a = 0.f;
+        
     for(int x_ = x_from; x_ <= x_to; ++x_)
     {
         for(int y_ = y_from; y_ <= y_to; ++y_)
         {            
-            b += src[threadIdx.x + 4 * x_ + 4 * width*y_] / coef;
+            dst[real_x + width * real_y].r += src[x_ + width*y_].r / coef;
+            dst[real_x + width * real_y].g += src[x_ + width*y_].g / coef;
+            dst[real_x + width * real_y].b += src[x_ + width*y_].b / coef;
+            dst[real_x + width * real_y].a += src[x_ + width*y_].a / coef;
         }
     }
-    dst[threadIdx.x + 4 * blockIdx.x + 4 * width * blockIdx.y] = b;
+    //dst[blockIdx.x + width * blockIdx.y] = b;
 }
-
 t_timing
 gpu_filter
 (
@@ -59,7 +68,7 @@ gpu_filter
 )
 {
     t_timing result;
-    float *gpu_src, *gpu_dst;
+    t_RGBA *gpu_src, *gpu_dst;
     {
         ScopeTimer _dummy(&(result.utilities_time));
         HANDLE_ERROR(cudaMalloc(&gpu_src, width*height*depth*sizeof(t_RGBA)));
@@ -73,17 +82,18 @@ gpu_filter
         }
         {
             
-            dim3 grid(width, height);
+            dim3 grid(width, height/TPB);
             for(int i = 0; i < repetitions; ++i)
             {
                 {
                     ScopeTimer _dummy(&(result.processing_time));
-                    gpu_kernel << < grid, 4 >> > (gpu_src, gpu_dst, width, height, depth);
+                    gpu_kernel << < grid, TPB >> > (gpu_src, gpu_dst, width, height, depth);
                     HANDLE_ERROR(cudaGetLastError());
-                }
-                {
-                    ScopeTimer _dummy(&(result.transfer_time));
-                    HANDLE_ERROR(cudaMemcpy(gpu_src, gpu_dst, width*height*depth*sizeof(t_RGBA), cudaMemcpyDeviceToDevice));
+                
+                    t_RGBA *tmp;
+                    tmp = gpu_src;
+                    gpu_src = gpu_dst;
+                    gpu_dst = tmp;
                 }
             }        
         }
